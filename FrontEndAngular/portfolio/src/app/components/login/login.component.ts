58cf26a6139260a2f5c2cd0e280219df3d1f35d7
emilio <emilioemoran@gmail.com>
2022-05-13 14:55:43 -0300

    repo subido a firebase

--- FrontEndAngular/portfolio/src/app/components/login/login.component.ts
+++ FrontEndAngular/portfolio/src/app/components/login/login.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 import { Router } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rx';
 
 @Component({
   selector: 'app-login',
@@ -19,29 +17,12 @@ export class LoginComponent implements OnInit {
     private route: Router
   ) {
     this.form = this.formBuilder.group({
-      /*  id: [''],
-      username: [''],
-      email: ['', [Validators.required, Validators.email]],
-      accessToken: [''],
-      tokenType: [''], */
       username: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
-      /* email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-       deviceInfo: this.formBuilder.group({
-        deviceId: [''],
-        deviceType: [''],
-        notificationToken: [''],
-      }), */
     });
   }
 
   ngOnInit(): void {}
-
-  get Email() {
-    return this.form.get('email');
-  }
 
   get Password() {
     return this.form.get('password');
